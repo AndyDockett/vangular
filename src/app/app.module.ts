f6e2e863cdee26fd2362a22d20248d347382f806
andydockett <adockett@artemedios.com>
2017-08-08 12:09:22 -0400

    Added full width
    museums comp.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+
+import { Wpng2RoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { PostSingleComponent } from './posts/post-single/post-single.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponent,
+    PostSingleComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [  
+  	BrowserModule,
+  	HttpModule,
+    Wpng2RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
